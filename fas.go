854178a9ace215152decab1a8aefb4271193de95
Paul Crane <paul@acheronsoft.com>
2021-05-19 01:15:30 -0500

    Add main.go
    This splits the anime scene code into its own file fas.go.

--- fas.go
+++ fas.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-
-	"github.com/Soulsbane/goapp/pkg/cli"
-	"github.com/pelletier/go-toml"
-)
-
 type anime struct {
 	Anime  string  `toml:"anime"`
 	Scenes []Scene `toml:"scene"`
@@ -20,25 +11,4 @@ type Scene struct {
 	Episode     string
 	Time        string
 	Description string
-}
-
-var args struct {
-	Quiet bool `arg:"-q"` // this flag is global to all subcommands
-}
-
-func main() {
-	var favorites anime
-	app := cli.NewCmdLineApp("Test App", "1.0", &args)
-
-	app.PrintWarning("This is alpha software. Use at your own risk!")
-	content, _ := ioutil.ReadFile("test.toml")
-
-	if err := toml.Unmarshal(content, &favorites); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Anime: ", favorites.Anime)
-	for _, s := range favorites.Scenes {
-		fmt.Printf("%s (%s) - %s\n", s.Name, s.Episode, s.Time)
-	}
 }
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/Soulsbane/goapp/pkg/cli"
+	"github.com/pelletier/go-toml"
+)
+
+var args struct {
+	Quiet bool `arg:"-q"` // this flag is global to all subcommands
+}
+
+func main() {
+	var favorites anime
+	app := cli.NewCmdLineApp("Test App", "1.0", &args)
+
+	app.PrintWarning("This is alpha software. Use at your own risk!")
+	content, _ := ioutil.ReadFile("test.toml")
+
+	if err := toml.Unmarshal(content, &favorites); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Anime: ", favorites.Anime)
+	for _, s := range favorites.Scenes {
+		fmt.Printf("%s (%s) - %s\n", s.Name, s.Episode, s.Time)
+	}
+}
